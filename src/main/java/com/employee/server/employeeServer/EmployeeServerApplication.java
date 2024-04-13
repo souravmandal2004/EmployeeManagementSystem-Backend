78fcbf6a25fe8b225e25161c457cf1137c56f709
souravmandal2004 <mandalsourav2004@gmail.com>
2024-04-13 17:58:06 +0530

    Get mapping added

--- src/main/java/com/employee/server/employeeServer/EmployeeServerApplication.java
+++ src/main/java/com/employee/server/employeeServer/EmployeeServerApplication.java
@@ -2,12 +2,18 @@ package com.employee.server.employeeServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class EmployeeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServerApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Welcome to Mandal's Backend Universe";
 	}
 
 }
